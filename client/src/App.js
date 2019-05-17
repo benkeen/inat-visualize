031561d9f946c9f86e22b0c7b7924e2365b33138
Ben Keen <ben.keen@gmail.com>
2019-05-16 21:49:51 -0700

    trying something new

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
-import Graph from './Graph';
 import './App.css';
 
 class App extends Component {
-	state = {
-		kingdoms: []
-	};
+	constructor (props) {
+		super(props);
 
-	componentDidMount() {
-		this.callApi()
-			.then(res => this.setState({ kingdoms: res }))
-			.catch(err => console.log(err));
+		this.search = this.search.bind(this);
+		this.onChangeObservationId = this.onChangeObservationId.bind(this);
 	}
 
-	callApi = async () => {
-		const response = await fetch('/api/taxa');
+	state = {
+		taxonIds: [],
+		nextObservationId: '',
+		data: []
+	};
+
+	callApi = async (observationId) => {
+		const response = await fetch(`/api/taxon?id=${observationId}`);
 		const body = await response.json();
 
 		if (response.status !== 200) {
@@ -24,17 +26,86 @@ class App extends Component {
 		return body;
 	};
 
-	render () {
-		const { kingdoms } = this.state;
+	getTables () {
+		return this.state.data.map((data, index) => (
+			<table class="table" key={index}>
+				<tbody>
+				{this.showTable(data)}
+				</tbody>
+			</table>
+		));
+	}
 
-		// if (!kingdoms.length) {
-		// 	return null;
-		// }
+	showTable (data) {
+		return data.ancestors.map((item) => (
+			<tr key={item.id}>
+				<td>
+					<img src={item.default_photo.square_url} width={32} height={32} alt=""/>
+				</td>
+				<td>
+					{item.rank}
+				</td>
+				<td>
+					{item.name}
+					<span>({item.observations_count})</span>
+				</td>
+			</tr>
+		));
+	}
+
+	search (e) {
+		e.preventDefault();
+
+		this.setState((state) => {
+
+			this.callApi(this.state.nextObservationId)
+				.then(res => {
+					this.setState((state) => ({
+						data: [...state.data, res]
+					}));
+				})
+				.catch(err => console.log(err));
+
+			return {
+				taxonIds: [...state.taxonIds, state.nextObservationId],
+				nextObservationId: ''
+			};
+		});
+	}
+
+	showSelectedPills () {
+		return this.state.taxonIds.map((taxonId) => (
+			<span className="pill">
+				{taxonId}
+				<span>x</span>
+			</span>
+		));
+	}
+
+	onChangeObservationId (e) {
+		this.setState({
+			nextObservationId: e.target.value
+		});
+	}
+
+	render () {
+		const { nextObservationId } = this.state;
 
 		return (
-			<div className="App">
-				<Graph kingdoms={kingdoms} />
-			</div>
+			<section>
+				<form onSubmit={this.search}>
+					<input type="text" size={10} value={nextObservationId} onChange={this.onChangeObservationId}/>
+					<input type="submit" value="Search" />
+				</form>
+
+				<span className="pill-block">
+				{this.showSelectedPills()}
+				</span>
+
+				<hr size="1" />
+
+				{this.getTables()}
+			</section>
 		);
 	}
 }
--- server.js
+++ server.js
@@ -6,32 +6,44 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const userId = 1148374; // me!
 const baseUrl = 'https://api.inaturalist.org/v1';
 
-app.get('/api/taxa', (req, res) => {
+// 215211 - variable
+// - alfalfa looper
+
+app.get('/api/taxon', (req, res) => {
+	const observationId = req.query.id;
 	Promise.all([
-		axios.get(`${baseUrl}/taxa?rank=kingdom`),
-		axios.get(`${baseUrl}/observations/species_counts?rank=kingdom&user_id=${userId}`)
+		axios.get(`${baseUrl}/taxa/${observationId}`)
 	]).then((values) => {
-		const found = {};
-		values[1].data.results.map(({ count, taxon }) => {
-			found[taxon.id] = { count };
-		});
-
-		const cleanData = {
-			name: 'Kingdoms',
-			children: values[0].data.results.map((taxon) => ({
-				name: taxon.name,
-				totalCount: taxon.observations_count,
-				numObservations: (found[taxon.id] && found[taxon.id].count) ? found[taxon.id].count : 0,
-				children: []
-			}))
-		};
-
-		res.send(cleanData);
+		res.send(values[0].data.results[0]);
 	});
 });
+
+// app.get('/api/taxa', (req, res) => {
+// 	Promise.all([
+// 		axios.get(`${baseUrl}/taxa?rank=kingdom`),
+// 		axios.get(`${baseUrl}/observations/species_counts?rank=kingdom&user_id=${userId}`),
+// 		axios.get(`${baseUrl}/observations/taxon_stats.json?user_id=${userId}`)
+// 	]).then((values) => {
+// 		const found = {};
+// 		values[1].data.results.map(({ count, taxon }) => {
+// 			found[taxon.id] = { count };
+// 		});
+//
+// 		const cleanData = {
+// 			name: 'Kingdoms',
+// 			children: values[0].data.results.map((taxon) => ({
+// 				name: taxon.name,
+// 				totalCount: taxon.observations_count,
+// 				numObservations: (found[taxon.id] && found[taxon.id].count) ? found[taxon.id].count : 0,
+// 				children: []
+// 			}))
+// 		};
+//
+// 		res.send(cleanData);
+// 	});
+// });
 
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
